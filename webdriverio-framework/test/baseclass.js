95da158973c20cf32a83c6bc3c7252ab56ddcc8f
Rama <rammohanreddy31@gmail.com>
2020-04-01 22:55:07 +0530

    Added webdriver io project

---
+++ webdriverio-framework/test/baseclass.js
@@ -0,0 +1,8 @@
+describe("Launching Application", function(){
+    it("Open URL", function(){
+        browser.url('/');
+
+        const search = $('#twotabsearchtextbox');
+        search.setValue('Mobiles');
+    })
+})
