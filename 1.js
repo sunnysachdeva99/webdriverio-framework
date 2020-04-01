b24117398218fc3dcadbb3ca110332be3bde0663
Raju Gade <raju.gade@kohls.com>
2020-04-01 19:08:13 +0530

    WebDriverIO Element APIs, click, sendKeys and getText Examples

--- 1.js
+++
@@ -1,7 +0,0 @@
-
-    var name=document.getElementById('apple').innerText;
-    alert(name);
-    var x=document.getElementsByClassName('iphone').length;
-  alert(x);  
-  
- 
---
+++ test/element.js
@@ -0,0 +1,25 @@
+describe("interaction with web elements", function(){
+
+    it("enter the values into text fields", function(){
+
+        browser.url('/');
+        const search = $('#twotabsearchtextbox');
+        search.setValue('Mac Book');
+        browser.pause(5000);
+
+        const lable = $('span.a-size-base.a-color-base');
+        console.log(lable.getText());
+        
+    });
+
+    it("click on a field", function(){
+        browser.url('/');
+        const search = $('#twotabsearchtextbox');
+        search.setValue('Mac Book');
+        const searchBtn = $('input.nav-input');
+        searchBtn.click();
+        browser.pause(5000);
+
+    });
+
+});
