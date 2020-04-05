9c9a082493d93f77dd433e9b96cf98c1b8ac96f8
SumantaKumar <sumantakumar.behera@kohls.com>
2020-04-05 23:16:48 +0530

    page object started

--- pages/blazepage.js
+++ pages/blazepage.js
@@ -1,19 +1,25 @@
-class BlazePage{
+class BlazePage {
 
-    get parent(){
+    get parent() {
         return $('ul.list-nav-links');
     }
 
-    get children(){
+    get children() {
         return this.parent.$$('li');
     }
 
-    get getTextForLi(){
+    get getTextForLi() {
         return this.children.filter(element => {
             console.log("Element Text: ==> ", element.getText());
         });
     }
 
+    // $$ directly used to minimize the above more methods
+    get mainLinksElementText() {
+        return $$('div.home_feature > a').forEach(element => {
+            console.log("Each Testing Platform Element Text: ==> ", element.getText());
+        });
+    }
 }
 
 module.exports = new BlazePage();
--- test/blazempagetest.js
+++ test/blazempagetest.js
@@ -6,6 +6,6 @@ describe('BlazePage elements handle', () =>{
         browser.url('https://www.blazemeter.com/');
         browser.pause(5000);
         blazePage.getTextForLi;
-    
+        blazePage.mainLinksElementText;
     });
 });
