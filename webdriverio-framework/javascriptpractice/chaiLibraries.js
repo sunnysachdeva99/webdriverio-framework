8a87e087ac30afec93482f60bbdec434814e586d
Rama <rammohanreddy31@gmail.com>
2020-04-09 21:32:49 +0530

    Committed chai libraries class

--- webdriverio-framework/javascriptpractice/chaiLibraries.js
+++ webdriverio-framework/javascriptpractice/chaiLibraries.js
@@ -1,17 +1,17 @@
 var expect = require('chai').expect
 
-// describe("String Assertions of Chai", function(){
-//     it("different type of String assertions", function(){
-//     expect('abc').to.be.a('string');
-//     console.log('1-its a string');
+describe("String Assertions of Chai", function(){
+    it("different type of String assertions", function(){
+    expect('abc').to.be.a('string');
+    console.log('1-its a string');
 
-//     expect('abc').not.to.be.a('number');
-//     console.log('2-its a string');
+    expect('abc').not.to.be.a('number');
+    console.log('2-its a string');
 
-//     expect('abcd').which.is.a('string');  
-//     console.log('3- its a string');
-//     })
-// })
+    expect('abcd').which.is.a('string');  
+    console.log('3- its a string');
+    })
+})
 
 describe("Number Assertions of chai",function(){
     it("different type of number assertions",function(){
@@ -32,44 +32,44 @@ describe("Number Assertions of chai",function(){
 })
 
 
-// describe("Object Assertions of chai",function(){
-//     it("different type of object assertions",function(){
+describe("Object Assertions of chai",function(){
+    it("different type of object assertions",function(){
       
-//     var details= {name:"Rama", age: 32};
+    var details= {name:"Rama", age: 32};
     
-//     expect(details).which.is.an('object');
+    expect(details).which.is.an('object');
 
-//     console.log('1-its a Object');
+    console.log('1-its a Object');
 
-//     expect(details).not.to.be.a('string');
-//     console.log('2- its a object');
+    expect(details).not.to.be.a('string');
+    console.log('2- its a object');
 
-//     expect(details).which.is.an('object').but.has.a.property('age');
-//     console.log('3-its a object');
+    expect(details).which.is.an('object').but.has.a.property('age');
+    console.log('3-its a object');
 
-//     expect(details).which.is.an('object').but.has.a.property('name');
-//     console.log('4- its a object');
+    expect(details).which.is.an('object').but.has.a.property('name');
+    console.log('4- its a object');
 
-//     expect(details).which.is.an('object').but.has.a.property('age').which.is.a('number');
-//     console.log('5-its a object');
+    expect(details).which.is.an('object').but.has.a.property('age').which.is.a('number');
+    console.log('5-its a object');
 
-//     expect(details).which.is.an('object').but.has.a.property('name').which.is.a('string');
-//     console.log('6-its a object');
+    expect(details).which.is.an('object').but.has.a.property('name').which.is.a('string');
+    console.log('6-its a object');
 
-//     })
-// })
+    })
+})
 
-// describe("Boolean Assertions of chai",function(){
-//     it("different type of boolean assertions",function(){
-//     expect(true).to.be.a('boolean');
-//     console.log('1-its a boolean');
+describe("Boolean Assertions of chai",function(){
+    it("different type of boolean assertions",function(){
+    expect(true).to.be.a('boolean');
+    console.log('1-its a boolean');
 
-//     expect(false).not.to.be.a('string');
-//     console.log('2- its boolean');
+    expect(false).not.to.be.a('string');
+    console.log('2- its boolean');
 
 
-//     })
-// })
+    })
+})
 
 describe("data Type of assertions",function(){
     var details= {name:"Rama", age: 32};
