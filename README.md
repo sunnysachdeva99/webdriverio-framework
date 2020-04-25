df3b20eeb7aa71992e1a17185558b25feb82f112
SUMANTA KUMAR <sumantakumarbehera@SUMANTAs-MacBook-Air.local>
2020-04-25 17:43:50 +0530

    firefox issue fixed

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # webdriverio-framework
 webdriverio based automation framework
+
+Selenium Grid Issue:
+
+Working fine:
+if no geckodriver installed in node_modules.
+All the dependencies are low then updated versions.
+
+Not working fine:
+if geckodriver installed in node_modules.
+All the dependencies are updated versions.
+
+Challenge to be looked into.
+
--- wdio.conf.js
+++ wdio.conf.js
@@ -31,12 +31,13 @@ exports.config = {
     // Override default path ('/wd/hub') for chromedriver service.
     hostname: process.env.GRID_HOST,
     port: Number(process.env.GRID_PORT),
-    path: process.env.GRID_PATH,
-    
+    // path: process.env.GRID_PATH,
+
     // hostname: '192.168.100.5',
     // hostname: 'localhost',
     // port: 4444,
     // path: '/wd/hub',
+
 
     // Protocol: http | https
     // protocol: 'http',
@@ -83,10 +84,10 @@ exports.config = {
         // 'path/to/excluded/files'
 
         //API Test
-        // './test/api-test.js', 
+        './test/api-test.js', 
 
         //UI Test
-        './test/hubspot-test.js'
+        // './test/hubspot-test.js'
     ],
     //
     // ============
@@ -113,31 +114,31 @@ exports.config = {
     capabilities: [
         {
 
-        // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-        // grid with only 5 firefox instances available you can make sure that not more than
-        // 5 instances get started at a time.
-        maxInstances: 2,
-        //
-        browserName: 'chrome',
-        "goog:chromeOptions": {
-            // to run chrome headless the following flags are required
-            // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
-            // args: ['--headless', '--disable-gpu'],       
-        }
-        // If outputDir is provided WebdriverIO can capture driver session logs
-        // it is possible to configure which logTypes to include/exclude.
-        // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-        // excludeDriverLogs: ['bugreport', 'server'],
+            // maxInstances can get overwritten per capability. So if you have an in-house Selenium
+            // grid with only 5 firefox instances available you can make sure that not more than
+            // 5 instances get started at a time.
+            maxInstances: 2,
+            browserName: 'chrome',
+            "goog:chromeOptions": {
+                // to run chrome headless the following flags are required
+                // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
+                // args: ['--headless', '--disable-gpu'],       
+            }
+            // If outputDir is provided WebdriverIO can capture driver session logs
+            // it is possible to configure which logTypes to include/exclude.
+            // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
+            // excludeDriverLogs: ['bugreport', 'server'],
 
-    }
-    // // ,{
-    //     maxInstances: 1,
-    //     browserName: 'firefox',
-    //     "moz:firefoxOptions": {
-    //         // flag to activate Firefox headless mode (see https://github.com/mozilla/geckodriver/blob/master/README.md#firefox-capabilities for more details about moz:firefoxOptions)
-    //         // args: ['-headless']
-    //     }
-    // }
+        }
+        ,
+        {
+            maxInstances: 1,
+            browserName: 'firefox',
+            "moz:firefoxOptions": {
+                // flag to activate Firefox headless mode (see https://github.com/mozilla/geckodriver/blob/master/README.md#firefox-capabilities for more details about moz:firefoxOptions)
+                // args: ['-headless', '-private']
+            }
+        }
     ],
     //
     // ===================
@@ -187,7 +188,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver'],
+    services: ['chromedriver', 'geckodriver'],
 
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
