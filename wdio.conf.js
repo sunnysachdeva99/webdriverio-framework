a3d54103bd0d684928ee1c3aaba165f4b6c3e107
SumantaKumar <sumantakumar.behera@kohls.com>
2020-05-06 16:53:06 +0530

    headless execution

--- wdio.conf.js
+++ wdio.conf.js
@@ -136,7 +136,7 @@ exports.config = {
             "goog:chromeOptions": {
                 // to run chrome headless the following flags are required
                 // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
-                // args: ['--headless', '--disable-gpu'],       
+                args: ['--headless', '--disable-gpu'],       
             }
             // If outputDir is provided WebdriverIO can capture driver session logs
             // it is possible to configure which logTypes to include/exclude.
